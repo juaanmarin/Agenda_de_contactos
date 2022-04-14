bf85eea68d794aba491796a58c5c38379037cd31
juan david marin <juaanmarin2007@gmail.com>
2022-04-14 11:26:17 -0500

    se añadieron los estilos a la pagina index y editar_contacto y se cambio el codigo del script

--- Script.sql
+++ Script.sql
@@ -7,10 +7,3 @@ create table contactos(
     telefono varchar(255) not null,
 	email varchar(255) not null
 );
-
-drop table contactos;
-
-/*consultas
-SELECT * FROM agenda_contactos.contactos;
-DELETE FROM `agenda_contactos`.`contactos` WHERE (`id` = '7');
-*/
